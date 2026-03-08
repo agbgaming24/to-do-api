5693d1f2c5ab9979d49c62ca75609a961942bc55
agbgaming24 <aryan2446.pis@gmail.com>
2026-03-08 16:18:53 +0530

    remove test

--- app.js
+++ app.js
@@ -10,14 +10,6 @@ app.use(express.json())
 app.use('/',authRoutes)
 app.use('/',todoRoutes)
 
-app.get('/test', (req, res) => {
-  res.json({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME
-  })
-})
 
 app.listen(port,()=>{
     console.log(`server running on http://localhost:${port}`);
